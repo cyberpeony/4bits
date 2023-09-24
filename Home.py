545fcb53d186e3ed19336d34c8e32c1531432b06
Fernanda Díaz Gutiérrez <110874443+cyberpeony@users.noreply.github.com>
2023-09-24 07:06:05 +0000

    botdisc

--- Home.py
+++ Home.py
@@ -1,4 +1,57 @@
 import streamlit as st
+import os
+import uuid
+from supabase import create_client, Client
+
+#Estilos de la pagina
+st.markdown(
+    """
+    <style>
+    %s
+    </style>
+    """ % open("Home.css").read(),
+    unsafe_allow_html=True
+)
+
+def load_css(file_name):
+    with open(file_name) as f:
+        st.markdown(f'<style>{f.read()}</style>', unsafe_allow_html=True)
+
+load_css('Home.css')
+#-------------------------
+
+# SUPABASE
+# Uses st.cache_resource to only run once.
+@st.cache_resource
+def init_connection():
+    url = st.secrets.supabase.SUPABASE_URL
+    key = st.secrets.supabase.SUPABASE_KEY
+    return create_client(url, key)
+
+supabase = init_connection()
+
+# Perform query.
+# Uses st.cache_data to only rerun when the query changes or after 10 min.
+# @st.cache_data(ttl=600)
+#def run_query():
+    #return supabase.table("mytable").select("*").execute()
+
+#rows = run_query()
+
+# Print results.
+#for row in rows.data:
+   #st.write(f"{row['name']} has a :{row['pet']}:")
+   
+email = st.text_input("Correo: ")
+password = st.text_input("Contraseña: ")
+res = supabase.auth.sign_up({
+  "email": email,
+  "password": password,
+})
+
+
+# data = supabase.auth.sign_in_with_password({"email": email, "password": password})
+
 
 
 # streamlit run /workspaces/4bits/Home.py
--- pages/2 - ChatBot.py
+++ pages/2 - ChatBot.py
@@ -1,13 +1,23 @@
 import streamlit as st
 import openai
 import os
-from supabase import create_client, Client
+
+#Estilos de la pagina
+st.markdown(
+    """
+    <style>
+    %s
+    </style>
+    """ % open("ChatStyles.css").read(),
+    unsafe_allow_html=True
+)
+#-------------------------
 
 #Mental health related keywords:
 mental_health_keywords = ["anxiety", "depression", "stress", "therapy"]
 
 def is_mental_health_related(input_text):
-    input_text = input_text.lower()
+    input_text = input_text
     return any(keyword in input_text for keyword in mental_health_keywords)
 
 
@@ -15,10 +25,12 @@ openai_api_key = st.secrets.openai.OPENAI_API_KEY
 openai.api_key = openai_api_key
 
 messages = [
-    {"role": "system", "content": "You are a kind helpful assistant."},
+    {"role": "system", "content": "Entiendo que estés pasando por un momento difícil. Por favor, espera un momento y te conectaré con alguien que pueda ayudarte. Es importante mencionar que soy un intermediario y no tengo la capacidad de entender o responder a situaciones complejas como un ser humano. Si sientes que estás en peligro inmediato, por favor, contacta a las autoridades locales o busca atención médica urgente."},
 ]
 
-message = st.text_input("User: ")
+message = st.chat_input("¿Cómo te sientes?: ")
+
+#if is_mental_health_related(message):
 
 if message:
     messages.append(
@@ -30,30 +42,15 @@ if message:
     )
 
     reply = chat.choices[0].message.content
-    st.write(f"ChatGPT: {reply}")
+    #st.write(f"ChatGPT: {reply}")
     messages.append({"role": "assistant", "content": reply})
 
+    for message in messages:
+        with st.chat_message(message["role"]):
+            st.markdown(message["content"])
+#else:
+#    st.text("Please enter a mental health-related question.")
 
 st.title("AuroraBot")
 
-# Initialize connection.
-# Uses st.cache_resource to only run once.
-@st.cache_resource
-def init_connection():
-    url = st.secrets.supabase.SUPABASE_URL
-    key = st.secrets.supabase.SUPABASE_KEY
-    return create_client(url, key)
 
-supabase = init_connection()
-
-# Perform query.
-# Uses st.cache_data to only rerun when the query changes or after 10 min.
-@st.cache_data(ttl=600)
-def run_query():
-    return supabase.table("mytable").select("*").execute()
-
-rows = run_query()
-
-# Print results.
-for row in rows.data:
-    st.write(f"{row['name']} has a :{row['pet']}:")
